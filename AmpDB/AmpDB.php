ec64f4c94220bd56822e090ff6fd235d90d5c95d
Faztasio <zack@fbts.ca>
2019-01-09 10:30:14 -0800

    Update

--- AmpDB/AmpDB.php
+++ AmpDB/AmpDB.php
@@ -1,3 +1,10 @@
 <?php
-
+function ampdb_create($name)
+{
+  return fopen("./" . $name . ".json", "a");
+}
+function ampdb_set($db, $key, $value)
+{
+  $contents = fread(fopen($db, "a"), filesize($db))
+}
 ?>
