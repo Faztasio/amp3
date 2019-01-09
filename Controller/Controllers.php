0e63c718cf3278eae757488b68fe83909940aa02
Faztasio <zack@fbts.ca>
2019-01-09 10:48:23 -0800

    Update

--- Controller/Controllers.php
+++ Controller/Controllers.php
@@ -4,8 +4,11 @@ $Controller = new Controller();
 
 $Controller->Create("amp3.homepage", function($ampdb, $route)
 {
+  $ampdb.Create("Amp3")
   if($route[0] === "set") {
-    $
+    $key = $route[1];
+    $val = $route[2];
+    
   }
 })
 ?>
