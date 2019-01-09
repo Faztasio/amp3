fc4e133eccd6a46b2b02070518583e20731dbe0d
Faztasio <zack@fbts.ca>
2019-01-09 10:46:26 -0800

    Update

--- Controller/Controllers.php
+++ Controller/Controllers.php
@@ -1,11 +1,9 @@
 <?php
 include "./Controller.php";
+$Controller = new Controller();
 
-$Controller->Create('amp3.homepage', function($ampdb, $route)
+$Controller->Create("amp3.homepage", function($ampdb, $route)
 {
-  $ampdb.Create($route[0]);
-  $ampdb.Set($route[0], $route[1], $route[2]);
-  echo implode($route)
-  echo ampdb.Get($route[0], $route[1]);
+  if($route[0] === "")
 })
 ?>
