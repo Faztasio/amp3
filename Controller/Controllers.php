de1a06ca0a645b28092c31d423521f459de0f1ed
Faztasio <zack@fbts.ca>
2019-01-09 10:45:22 -0800

    Update

--- Controller/Controllers.php
+++ Controller/Controllers.php
@@ -5,7 +5,7 @@ $Controller->Create('amp3.homepage', function($ampdb, $route)
 {
   $ampdb.Create($route[0]);
   $ampdb.Set($route[0], $route[1], $route[2]);
-  echo implode(rout)
+  echo implode($route)
   echo ampdb.Get($route[0], $route[1]);
 })
 ?>
