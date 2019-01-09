bb90b39c39dca7d77fc70a173fe99420474daedd
Faztasio <zack@fbts.ca>
2019-01-09 11:20:40 -0800

    Update

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ require 'vendor/autoload.php';
 require 'LoadCore.php';
 $app = new Init;
 define('ENVIRONMENT', 'production');
-
-//
+/*
+*---------------------------------------------------------------
+* ERROR REPORTING
+*---------------------------------------------------------------
+*
+* Different environments will require different levels of error reporting.
+* By default development will show errors but testing and live will hide them.
+*/
 if (defined('ENVIRONMENT'))
 	{
 		switch (ENVIRONMENT)
